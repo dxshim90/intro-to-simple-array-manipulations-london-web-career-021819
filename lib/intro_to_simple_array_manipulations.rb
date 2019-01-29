790d04320c9cf286702175b864f782751b584f79
Daniel Shimield <dxshim90@gmail.com>
2019-01-29 17:24:52 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,7 @@ def using_push(array, element)
   array.push(element)
 end
 
-def using_unshift(array, "element")
-array.unshift("element")
-array.length
+def using_unshift(array, element)
+array.unshift(element)
 end
 
